dd16709a9475cbc285d797a7efa7ac2fbdab1653
eSaints <esaints@proton.me>
2025-06-16 23:07:47 +0200

    Final v5 Build Fix Package: Exports cleanup + MintButton imports

--- src/lib/contractConfig.ts
+++ src/lib/contractConfig.ts
@@ -1,4 +1,4 @@
-export const ACCESS_PASS_ADDRESS = '0x453e51a953Fa5178bE4f043adf80409Bd3dCDDef'; // tvoj deployed contract na Polygonu
+export const ACCESS_PASS_ADDRESS = '0x453e51a953Fa5178bE4f043adf80409Bd3dCDDef';
 
 export const ACCESS_PASS_ABI = [
   {
